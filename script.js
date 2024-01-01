e73d77de6b8475d6f89d537a3f8ca843840fe936
MuaazArshad <mooeez.arshad@gmail.com>
2024-01-01 16:11:19 +0500

    Added

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const btns = document.querySelectorAll('.btn');
+const answers = document.querySelectorAll('.answer');
+
+btns.forEach((btn) => {
+	btn.addEventListener('click', (e) => {
+		btn.children[0].classList.toggle('fa-add');
+		btn.children[0].classList.toggle('fa-subtract');
+		btns.forEach((btn) => {
+			if (btn !== e.currentTarget) {
+				btn.children[0].classList.add('fa-add');
+				btn.children[0].classList.remove('fa-subtract');
+			}
+		});
+		answers.forEach((answer) => {
+			if (answer !== e.currentTarget.parentElement.nextElementSibling)
+				answer.classList.remove('active');
+		});
+		e.currentTarget.parentElement.nextElementSibling.classList.toggle('active');
+	});
+});
